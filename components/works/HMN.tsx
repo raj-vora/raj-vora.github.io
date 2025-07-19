a4dcc95435d34a45f574e468907a7747c34869ec
Raj Vora <rajvora@ufl.edu>
2025-07-19 14:16:48 -0400

    update HMN experience

--- components/works/HMN.tsx
+++ components/works/HMN.tsx
@@ -10,7 +10,7 @@ const HMN = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Software Engineer
+        Full Stack Engineer
         <a href="https://url.rajvora.tech/hmn" target="_blank"><span className="text-textGreen tracking-wide">@Human Motor Neuroscience Lab</span></a>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
@@ -18,34 +18,38 @@ const HMN = () => {
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
-          <span className="text-textGreen mt-1">
-            <TiArrowForward />
+          <span className="text-textGreen mt-1"><TiArrowForward /></span>
+          <span>
+            Accelerating research workflow for 5 person PhD team by speeding up data processing by 60%.
           </span>
-          Innovated 3 Flutter apps for recording neuromuscular and audio information, innovating data collection methods.
         </li>
+
         <li className="text-base flex gap-2 text-textDark">
-          <span className="text-textGreen mt-1">
-            <TiArrowForward />
+          <span className="text-textGreen mt-1"><TiArrowForward /></span>
+          <span>
+            Leveraging <span className="text-textGreen inline">AWS Amplify</span> for seamless data integration, built a NextJS dashboard for data analysis and modelling.
           </span>
-          Designed responsive mobile UIs in Figma with 15+ screens, prioritizing user experience and patient navigation.
         </li>
+
         <li className="text-base flex gap-2 text-textDark">
-          <span className="text-textGreen mt-1">
-            <TiArrowForward />
+          <span className="text-textGreen mt-1"><TiArrowForward /></span>
+          <span>
+            Automating data analysis using <span className="text-textGreen inline">Python and MATLAB</span> algorithms, resulting in a <span className="text-textGreen inline">30%</span> faster hypotheses validation.
           </span>
-          Integrated 10+ REST APIs employing Laravel backend, ensuring reliable collection and consistency with SQL database.
         </li>
+
         <li className="text-base flex gap-2 text-textDark">
-          <span className="text-textGreen mt-1">
-            <TiArrowForward />
+          <span className="text-textGreen mt-1"><TiArrowForward /></span>
+          <span>
+            Innovated <span className="text-textGreen inline">3 Flutter</span> apps to collect neuromuscular and phonetic information, leading to <span className="text-textGreen inline">50%</span> efficient data collection.
           </span>
-          Leveraging AWS Amplify for seamless data integration, built a NextJS dashboard for data processing and modelling.
         </li>
+
         <li className="text-base flex gap-2 text-textDark">
-          <span className="text-textGreen mt-1">
-            <TiArrowForward />
+          <span className="text-textGreen mt-1"><TiArrowForward /></span>
+          <span>
+            Crafted user-centric, responsive mobile interfaces in <span className="text-textGreen inline">Figma</span>, driving a <span className="text-textGreen inline">60%</span> improvement in overall user experience.
           </span>
-          Conducted user testing to refine features, applying object-oriented principles and user interaction refinements.
         </li>
       </ul>
     </motion.div>
